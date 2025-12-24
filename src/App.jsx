ee6e5d2d5a480cd94d623a91a14a2e3709d2227a
Fernando Bondar <fernandobondar@192.168.1.6>
2025-12-23 22:45:56 -0300

    Rename ProfessionalRegister to force rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import ProfessionalProfile from './pages/ProfessionalProfile';
+import ProfessionalProfile from './pages/ProfessionalProfile.jsx/index.js';
 import RatingForm from './pages/RatingForm';
-import QRResolve from './pages/QRResolve';
+import QRResolve from './pages/QRResolve.jsx';
 import ProfessionalDashboard from './pages/ProfessionalDashboard';
+import ClientDashboard from './pages/ClientDashboard';
 import SearchProfessionals from './pages/SearchProfessionals';
 import ProfessionalLogin from './pages/ProfessionalLogin';
-import ProfessionalRegister from './pages/ProfessionalRegister';
+import ProfessionalRegister from './pages/ProfessionalRegister2';
 import EditCV from './pages/EditCV';
 import Stats from './pages/Stats';
 
@@ -19,6 +20,8 @@ function App() {
         <Route path="/rate/:code" element={<QRResolve />} />
         <Route path="/rate-professional/:professionalId" element={<RatingForm />} />
         <Route path="/dashboard" element={<ProfessionalDashboard />} />
+        <Route path="/professional-dashboard" element={<ProfessionalDashboard />} />
+        <Route path="/client-dashboard" element={<ClientDashboard />} />
         <Route path="/search" element={<SearchProfessionals />} />
         <Route path="/professional-login" element={<ProfessionalLogin />} />
         <Route path="/professional-register" element={<ProfessionalRegister />} />
