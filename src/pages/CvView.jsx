7be320529ad1047aab1ec34647055161a5da05a8
siemprestereo <fernando.bondar@gmail.com>
2026-01-17 13:51:31 -0300

    fix CvView

--- src/pages/CvView.jsx
+++ src/pages/CvView.jsx
@@ -350,7 +350,7 @@ function CvView() {
             <svg className="w-6 h-6 text-green-600 mx-auto mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
             </svg>
-            <p className="font-semibold text-gray-800 text-sm">Compartir</p>
+            <p className="font-semibold text-gray-800 text-sm">Compartir CV</p>
           </button>
 
           <button
@@ -360,7 +360,7 @@ function CvView() {
             <svg className="w-6 h-6 text-blue-600 mx-auto mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
             </svg>
-            <p className="font-semibold text-gray-800 text-sm">PDF</p>
+            <p className="font-semibold text-gray-800 text-sm">Descargar CV en PDF</p>
           </button>
         </div>
       </div>
